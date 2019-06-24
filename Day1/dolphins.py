0cc57c755becbd927b27da79628e2fe746c399af
Daryl DeFord <daryl.r.deford@gmail.com>
2019-06-24 13:10:30 -0400

    Update dolphins.py

--- Day1/dolphins.py
+++ Day1/dolphins.py
@@ -67,3 +67,15 @@ A=[[0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 
 g_dolphin =nx.from_numpy_matrix(np.matrix(A))
 g_karate = nx.karate_club_graph()
+
+plt.figure()
+d = dict(g_dolphin.degree)
+nx.draw(g_dolphin,  node_size=[v * 100 for v in d.values()])
+plt.show()
+
+plt.figure()
+nx.draw(g_dolphin,  node_color=[v * 100 for v in d.values()],cmap='jet')
+plt.show()
+
+
+

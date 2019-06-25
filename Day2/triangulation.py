40297d5bd8fb8feb808858c48d9b37b39dc0afe1
Daryl DeFord <daryl.r.deford@gmail.com>
2019-06-25 12:44:24 -0400

    added distance plot version

--- Day2/triangulation.py
+++ Day2/triangulation.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 import numpy as np
 import networkx as nx
+import math
 
 a = 1 
 b = 1
 num_points = 20
+radius = .2
 
 points = np.random.rand(num_points, 2)
 points[:, 0] = a*points[:, 0] 
@@ -27,5 +29,25 @@ for n in range(num_points):
     
 pos = { n : points[n, :] for n in range(num_points)}
 
+plt.figure()
 nx.draw(g,pos=pos)
 plt.show()
+
+print(g.nodes())
+
+h = nx.Graph()
+
+
+for n1 in g.nodes():
+    h.add_node(n1)
+    for n2 in g.nodes():
+        if n1!=n2:
+            if math.sqrt((pos[n1][0]-pos[n2][0])**2+(pos[n1][1]-pos[n2][1])**2) < radius:
+                h.add_edge(n1,n2)
+                
+                
+plt.figure()
+nx.draw(h,pos=pos)
+plt.show()           
+
+
